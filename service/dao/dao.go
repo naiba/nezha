03f4baa75fa6b6e8503394736821c6b2bcfd04c0
naiba <hi@nai.ba>
2021-02-05 09:31:06 +0800

    💄 移动端登录表单居中

--- service/dao/dao.go
+++ service/dao/dao.go
@@ -13,7 +13,7 @@ import (
 	pb "github.com/naiba/nezha/proto"
 )
 
-var Version = "v0.4.5" // ！！记得修改 README 重的 badge 版本！！
+var Version = "v0.4.6" // ！！记得修改 README 重的 badge 版本！！
 
 const (
 	SnapshotDelay = 3
