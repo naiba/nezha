88e412fda665d851843f52a1ffe112b74553e588
naiba <hi@nai.ba>
2021-08-06 11:48:34 +0800

    其他按钮、文字等配色细节

--- resource/static/main.js
+++ resource/static/main.js
@@ -7,7 +7,7 @@ function readableBytes(bytes) {
   return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + sizes[i];
 }
 
-const confirmBtn = $(".mini.confirm.modal .positive.button");
+const confirmBtn = $(".mini.confirm.modal .nezha-primary-btn.button");
 
 function showConfirm(title, content, callFn, extData) {
   const modal = $(".mini.confirm.modal");
@@ -34,7 +34,7 @@ function showFormModal(modelSelector, formID, URL, getData) {
       closable: true,
       onApprove: function () {
         let success = false;
-        const btn = $(modelSelector + " .positive.button");
+        const btn = $(modelSelector + " .nezha-primary-btn.button");
         const form = $(modelSelector + " form");
         if (btn.hasClass("loading")) {
           return success;
@@ -106,7 +106,7 @@ function addOrEditAlertRule(rule) {
   const modal = $(".rule.modal");
   modal.children(".header").text((rule ? "修改" : "添加") + "报警规则");
   modal
-    .find(".positive.button")
+    .find(".nezha-primary-btn.button")
     .html(
       rule ? '修改<i class="edit icon"></i>' : '添加<i class="add icon"></i>'
     );
@@ -125,7 +125,7 @@ function addOrEditNotification(notification) {
   const modal = $(".notification.modal");
   modal.children(".header").text((notification ? "修改" : "添加") + "通知方式");
   modal
-    .find(".positive.button")
+    .find(".nezha-primary-btn.button")
     .html(
       notification
         ? '修改<i class="edit icon"></i>'
@@ -159,7 +159,7 @@ function addOrEditServer(server) {
   const modal = $(".server.modal");
   modal.children(".header").text((server ? "修改" : "添加") + "服务器");
   modal
-    .find(".positive.button")
+    .find(".nezha-primary-btn.button")
     .html(
       server ? '修改<i class="edit icon"></i>' : '添加<i class="add icon"></i>'
     );
@@ -184,7 +184,7 @@ function addOrEditMonitor(monitor) {
   const modal = $(".monitor.modal");
   modal.children(".header").text((monitor ? "修改" : "添加") + "监控");
   modal
-    .find(".positive.button")
+    .find(".nezha-primary-btn.button")
     .html(
       monitor ? '修改<i class="edit icon"></i>' : '添加<i class="add icon"></i>'
     );
@@ -223,7 +223,7 @@ function addOrEditCron(cron) {
   const modal = $(".cron.modal");
   modal.children(".header").text((cron ? "修改" : "添加") + "计划任务");
   modal
-    .find(".positive.button")
+    .find(".nezha-primary-btn.button")
     .html(
       cron ? '修改<i class="edit icon"></i>' : '添加<i class="add icon"></i>'
     );
