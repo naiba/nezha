c0f46c98f14de0048c2f4c40ff54ebb33b383b39
naiba <hi@nai.ba>
2021-09-29 20:14:25 +0800

    ✅ 更新IP脱敏测试用例 [skip ci]

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -14,24 +14,24 @@ type testSt struct {
 func TestNotification(t *testing.T) {
 	cases := []testSt{
 		{
-			input:  "ip(v4:103.80.236.249,v6:[d5ce:d811:cdb8:067a:a873:2076:9521:9d2d])",
-			output: "ip(v4:103.****.249,v6:[d5ce:d811:****:9521:9d2d])",
+			input:  "103.80.236.249/d5ce:d811:cdb8:067a:a873:2076:9521:9d2d",
+			output: "103.****.249/d5ce:d811:****:9521:9d2d",
 		},
 		{
-			input:  "ip(v4:3.80.236.29,v6:[d5ce::cdb8:067a:a873:2076:9521:9d2d])",
-			output: "ip(v4:3.****.29,v6:[d5ce::****:9521:9d2d])",
+			input:  "3.80.236.29/d5ce::cdb8:067a:a873:2076:9521:9d2d",
+			output: "3.****.29/d5ce::****:9521:9d2d",
 		},
 		{
-			input:  "ip(v4:3.80.236.29,v6:[d5ce::cdb8:067a:a873:2076::9d2d])",
-			output: "ip(v4:3.****.29,v6:[d5ce::****::9d2d])",
+			input:  "3.80.236.29/d5ce::cdb8:067a:a873:2076::9d2d",
+			output: "3.****.29/d5ce::****::9d2d",
 		},
 		{
-			input:  "ip(v4:3.80.236.9,v6:[d5ce::cdb8:067a:a873:2076::9d2d])",
-			output: "ip(v4:3.****.9,v6:[d5ce::****::9d2d])",
+			input:  "3.80.236.9/d5ce::cdb8:067a:a873:2076::9d2d",
+			output: "3.****.9/d5ce::****::9d2d",
 		},
 		{
-			input:  "ip(v4:3.80.236.9,v6:[d5ce::cdb8:067a:a873:2076::9d2d])",
-			output: "ip(v4:3.****.9,v6:[d5ce::****::9d2d])",
+			input:  "3.80.236.9/d5ce::cdb8:067a:a873:2076::9d2d",
+			output: "3.****.9/d5ce::****::9d2d",
 		},
 	}
 
