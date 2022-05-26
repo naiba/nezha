4f05bb8b2cc7c01a50f34956a42548f9d84fe4f8
Erope <wjhjd163@gmail.com>
2022-05-26 10:55:14 +0800

    JSdelivr -> Jihulab

--- script/install.sh
+++ script/install.sh
@@ -75,7 +75,7 @@ pre_check() {
         Get_Docker_Argu=" "
         Docker_IMG="ghcr.io\/naiba\/nezha-dashboard"
     else
-        GITHUB_RAW_URL="fastly.jsdelivr.net/gh/naiba/nezha@master"
+        GITHUB_RAW_URL="jihulab.com/nezha/nezha/-/raw/master"
         GITHUB_URL="dn-dao-github-mirror.daocloud.io"
         Get_Docker_URL="get.daocloud.io/docker"
         Get_Docker_Argu=" -s docker --mirror Aliyun"
