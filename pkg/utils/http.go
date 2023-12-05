813b813e5cf255d6d823d3105686847515be1f74
Mmx233 <admin@mmxblog.com>
2023-12-05 22:24:31 +0800

    feat: add pkg/utils/http HttpClientSkipTlsVerify HttpClient

---
+++ pkg/utils/http.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+)
+
+var (
+	HttpClientSkipTlsVerify *http.Client
+	HttpClient              *http.Client
+)
+
+func init() {
+	HttpClientSkipTlsVerify = httpClient(_httpClient{
+		Transport: httpTransport(_httpTransport{
+			VerifySSL: true,
+		}),
+	})
+	HttpClient = httpClient(_httpClient{
+		Transport: httpTransport(_httpTransport{
+			VerifySSL: false,
+		}),
+	})
+}
+
+type _httpTransport struct {
+	VerifySSL bool
+}
+
+func httpTransport(conf _httpTransport) *http.Transport {
+	return &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: conf.VerifySSL},
+	}
+}
+
+type _httpClient struct {
+	Transport *http.Transport
+}
+
+func httpClient(conf _httpClient) *http.Client {
+	return &http.Client{
+		Transport: conf.Transport,
+		Timeout:   time.Minute * 10,
+	}
+}
