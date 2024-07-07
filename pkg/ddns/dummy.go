4df60c69555238e63a10d8df5c364b7eabb57b84
UUBulb <35923940+uubulb@users.noreply.github.com>
2024-07-07 10:19:58 +0800

    ddns: improve performance (#385)

--- pkg/ddns/dummy.go
+++ pkg/ddns/dummy.go
@@ -2,6 +2,6 @@ package ddns
 
 type ProviderDummy struct{}
 
-func (provider ProviderDummy) UpdateDomain(domainConfig *DomainConfig) bool {
+func (provider *ProviderDummy) UpdateDomain(domainConfig *DomainConfig) bool {
 	return false
 }
