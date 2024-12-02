75d3989b3daf25c9ef04c9145c74f7d4298bd062
hiDandelion <77157418+hiDandelion@users.noreply.github.com>
2024-12-02 11:31:25 +0800

    fix: AlertRuleForm missing omitempty

--- model/alertrule_api.go
+++ model/alertrule_api.go
@@ -1,11 +1,11 @@
 package model
 
 type AlertRuleForm struct {
-	Name                string   `json:"name" minLength:"1"`
-	Rules               []Rule   `json:"rules"`
-	FailTriggerTasks    []uint64 `json:"fail_trigger_tasks"`    // 失败时触发的任务id
-	RecoverTriggerTasks []uint64 `json:"recover_trigger_tasks"` // 恢复时触发的任务id
-	NotificationGroupID uint64   `json:"notification_group_id"`
-	TriggerMode         uint8    `json:"trigger_mode" default:"0"`
-	Enable              bool     `json:"enable" validate:"optional"`
+	Name                string   `json:"name,omitempty" minLength:"1"`
+	Rules               []Rule   `json:"rules,omitempty"`
+	FailTriggerTasks    []uint64 `json:"fail_trigger_tasks,omitempty"`    // 失败时触发的任务id
+	RecoverTriggerTasks []uint64 `json:"recover_trigger_tasks,omitempty"` // 恢复时触发的任务id
+	NotificationGroupID uint64   `json:"notification_group_id,omitempty"`
+	TriggerMode         uint8    `json:"trigger_mode,omitempty" default:"0"`
+	Enable              bool     `json:"enable,omitempty" validate:"optional"`
 }
