1827963a8bfb7fc3ac28b1984f7d10954334ca2d
naiba <hi@nai.ba>
2024-12-19 23:21:15 +0800

    fix: rule snapshot

--- model/alertrule_api.go
+++ model/alertrule_api.go
@@ -2,7 +2,7 @@ package model
 
 type AlertRuleForm struct {
 	Name                string   `json:"name" minLength:"1"`
-	Rules               []Rule   `json:"rules"`
+	Rules               []*Rule  `json:"rules"`
 	FailTriggerTasks    []uint64 `json:"fail_trigger_tasks"`    // 失败时触发的任务id
 	RecoverTriggerTasks []uint64 `json:"recover_trigger_tasks"` // 恢复时触发的任务id
 	NotificationGroupID uint64   `json:"notification_group_id"`
